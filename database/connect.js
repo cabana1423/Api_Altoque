8c13fd634692dd5bcaff6978818bb0a74e22ceae
cabana <cabanaxsiempre@gmail.com>
2021-07-22 17:40:06 -0400

    solo por heroku2

--- database/connect.js
+++ database/connect.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://cabana_2:<Mundo1423>@pruebas.9mauq.mongodb.net/proyect?retryWrites=true&w=majority", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://cabana_2:Mundo1423@pruebas.9mauq.mongodb.net/proyect?retryWrites=true&w=majority", {useNewUrlParser: true});
 var db  = mongoose.connection;
 db.on("error", () => {
     console.log("ERRO no se puede conectar al servidor");
