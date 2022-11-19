21bf5ccf4e4c62129a09f497f160b9be031e114d
cabana1423 <cabanaxsiempre@gmail.com>
2022-11-19 03:35:20 -0400

    correcciones  presentacion 4

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node
+FROM node:18.12.1
 RUN mkdir -p /opt/app
 WORKDIR /opt/app
 COPY package.json .
--- database/connect.js
+++ database/connect.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://172.22.0.2:27017/based");
+mongoose.connect('mongodb+srv://cabana_2:Mundo14231423@pruebas.9mauq.mongodb.net/proyect?retryWrites=true&w=majority', {useNewUrlParser: true});
 var db  = mongoose.connection;
 db.on("error", () => {
     console.log("ERRO no se puede conectar al servidor");
